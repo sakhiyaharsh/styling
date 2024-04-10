8396768e3ebaa6e39bf40e260d0e2c873a68b849
harshsakhiya.01 <sakhiya.harsh.01@gmail.com>
2024-04-10 15:37:40 +0530

    update

---
+++ convertio-3/js/custom.js
@@ -0,0 +1,36 @@
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
+
+if (bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    });
+}
+
+if (close) {
+    close.addEventListener('click', () => {
+        nav.classList.remove('active');
+    });
+}
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    function counter(id, start, end, duration) {
+        let obj = document.getElementById(id),
+            current = start,
+            range = end - start,
+            increment = end > start ? 1 : -1,
+            step = Math.abs(Math.floor(duration / range)),
+            timer = setInterval(() => {
+                current += increment;
+                obj.textContent = current;
+                if (current == end) {
+                    clearInterval(timer);
+                }
+            }, step);
+    }
+    counter("count1", 150, 300, 6000);
+    counter("count2", 100, 250, 5000);
+    counter("count3", 50, 150, 4500);
+});
